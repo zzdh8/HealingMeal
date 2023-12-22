fd2efe42400c752f3dc505f2bd0a2999e5581f1e
jin <ekwls20@gmail.com>
2023-12-22 17:44:22 +0900

    project_begin

---
+++ healingmeal/src/main/java/gsc/healingmeal/HealingmealApplication.java
@@ -0,0 +1,13 @@
+package gsc.healingmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingmealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealingmealApplication.class, args);
+	}
+
+}
---
+++ healingmeal/src/test/java/gsc/healingmeal/HealingmealApplicationTests.java
@@ -0,0 +1,13 @@
+package gsc.healingmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingmealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
