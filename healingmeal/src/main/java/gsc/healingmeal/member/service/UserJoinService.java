a8119d2d218603efe54873fe1d86bd532c2dad15
inhooo00 <chldlsgh0987@naver.com>
2023-12-26 16:49:36 +0900

    join

--- healingmeal/src/main/java/gsc/healingmeal/member/service/UserJoinService.java
+++ healingmeal/src/main/java/gsc/healingmeal/member/service/UserJoinService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserService {
+public class UserJoinService {
 
     private BCryptPasswordEncoder encoder;
     private final UserRepository userRepository;
@@ -59,56 +59,4 @@ public class UserService {
         userRepository.save(joinRequestDto.toEntity(encoder.encode(joinRequestDto.getPassword())));
     }
 
-    /**
-     *  로그인 기능
-     *  화면에서 LoginRequest(loginId, password)을 입력받아 loginId와 password가 일치하면 User return
-     *  loginId가 존재하지 않거나 password가 일치하지 않으면 null return
-     */
-    public User login(LoginRequestDto loginRequestDto) {
-        Optional<User> optionalUser = userRepository.findByLoginId((loginRequestDto.getLoginId()));
-
-        // loginId와 일치하는 User가 없으면 null return
-        if(optionalUser.isEmpty()) {
-            return null;
-        }
-
-        User user = optionalUser.get();
-
-        // 찾아온 User의 password와 입력된 password가 다르면 null return
-        if(!user.getPassword().equals(loginRequestDto.getPassword())) {
-            return null;
-        }
-
-        return user;
-    }
-
-    /**
-     * userId(Long)를 입력받아 User을 return 해주는 기능
-     * 인증, 인가 시 사용
-     * userId가 null이거나(로그인 X) userId로 찾아온 User가 없으면 null return
-     * userId로 찾아온 User가 존재하면 User return
-     */
-    public User getLoginUserById(Long userId) {
-        if(userId == null) return null;
-
-        Optional<User> optionalUser = userRepository.findById(userId);
-        if(optionalUser.isEmpty()) return null;
-
-        return optionalUser.get();
-    }
-
-    /**
-     * loginId(String)를 입력받아 User을 return 해주는 기능
-     * 인증, 인가 시 사용
-     * loginId가 null이거나(로그인 X) userId로 찾아온 User가 없으면 null return
-     * loginId로 찾아온 User가 존재하면 User return
-     */
-    public User getLoginUserByLoginId(String loginId) {
-        if(loginId == null) return null;
-
-        Optional<User> optionalUser = userRepository.findByLoginId(loginId);
-        if(optionalUser.isEmpty()) return null;
-
-        return optionalUser.get();
-    }
 }
