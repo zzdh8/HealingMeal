7e5c1a26295b3cbdcc829bea160a18a967c8e84d
inhooo00 <chldlsgh0987@naver.com>
2023-12-26 13:53:32 +0900

    member

--- healingmeal/src/main/java/gsc/healingmeal/member/domain/User.java
+++ healingmeal/src/main/java/gsc/healingmeal/member/domain/User.java
@@ -1,9 +1,6 @@
 package gsc.healingmeal.member.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     private String loginId;
