e695a55c8308ddf740c22073257b3ee90233c4b7
inhooo00 <chldlsgh0987@naver.com>
2023-12-31 16:45:37 +0900

    config 추가

--- healingmeal/src/main/java/gsc/healingmeal/member/config/PasswordEncoderConfig.java
+++ healingmeal/src/main/java/gsc/healingmeal/member/config/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package gsc.healingmeal.config;
+package gsc.healingmeal.member.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
