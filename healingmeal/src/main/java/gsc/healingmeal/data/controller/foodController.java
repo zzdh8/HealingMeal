8e34e7670c54ba3bd6111392f57a6fd75e468c66
inhooo00 <chldlsgh0987@naver.com>
2024-01-06 01:30:05 +0900

    foodController 작성

--- healingmeal/src/main/java/gsc/healingmeal/data/controller/foodController.java
+++ healingmeal/src/main/java/gsc/healingmeal/data/controller/foodController.java
@@ -3,9 +3,7 @@ package gsc.healingmeal.data.controller;
 import gsc.healingmeal.data.service.FoodService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- healingmeal/src/main/java/gsc/healingmeal/data/domain/Food.java
+++ healingmeal/src/main/java/gsc/healingmeal/data/domain/Food.java
@@ -1,10 +1,7 @@
 package gsc.healingmeal.data.domain;
 
-
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.Date;
 
 @Entity
 @Getter
@@ -12,124 +9,20 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Food {
+public class Food_2 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "food_id")
+    @Column(name = "food2_id")
     private Long id;
 
-    private String foodCode;
     private String foodName;
-    private String dataClassificationCode;
-    private String dataClassificationName;
-    private Long foodOriginCode;
-    private String foodOriginName;
-    private String foodMajorCategoryCode;
-    private String foodMajorCategoryName;
-    private String representativeFoodCode;
+    private String foodCategory;
     private String representativeFoodName;
-    private String foodMiddleCategoryCode;
-    private String foodMiddleCategoryName;
-    private String foodSubCategoryCode;
-    private String foodSubCategoryName;
-    private String foodDetailCategoryCode;
-    private String foodDetailCategoryName;
-    private String nutrientContentStandardAmount;
-    private double energy;
-    private double water;
-    private double protein;
-    private double fat;
-    private double ash;
-    private double carbohydrate;
-    private double sugar;
-    private double dietaryFiber;
-    private double calcium;
-    private double iron;
-    private double phosphorus;
-    private double potassium;
-    private double sodium;
-    private double vitaminA;
-    private double retinol;
-    private double betaCarotene;
-    private double thiamine;
-    private double riboflavin;
-    private double niacin;
-    private double vitaminC;
-    private double vitaminD;
-    private double cholesterol;
-    private double saturatedFattyAcid;
-    private double transFattyAcid;
-    private Long sourceCode;
-    private String sourceName;
-    private String foodWeight;
-    private String company;
-    private Long dataCreationMethodCode;
-    private String dataCreationMethodName;
-    private String dataCreationDate;
-    private String dataBaseDate;
-    private Long providerCode;
-    private String providerName;
-
-
-    @Override
-    public String toString() {
-        return "Food{" +
-                "id=" + id +
-                "foodCode='" + foodCode + '\'' +
-                ", foodName='" + foodName + '\'' +
-                ", dataClassificationCode='" + dataClassificationCode + '\'' +
-                ", dataClassificationName='" + dataClassificationName + '\'' +
-                ", foodOriginCode=" + foodOriginCode +
-                ", foodOriginName='" + foodOriginName + '\'' +
-                ", foodMajorCategoryCode='" + foodMajorCategoryCode + '\'' +
-                ", foodMajorCategoryName='" + foodMajorCategoryName + '\'' +
-                ", representativeFoodCode='" + representativeFoodCode + '\'' +
-                ", representativeFoodName='" + representativeFoodName + '\'' +
-                ", foodMiddleCategoryCode='" + foodMiddleCategoryCode + '\'' +
-                ", foodMiddleCategoryName='" + foodMiddleCategoryName + '\'' +
-                ", foodSubCategoryCode='" + foodSubCategoryCode + '\'' +
-                ", foodSubCategoryName='" + foodSubCategoryName + '\'' +
-                ", foodDetailCategoryCode='" + foodDetailCategoryCode + '\'' +
-                ", foodDetailCategoryName='" + foodDetailCategoryName + '\'' +
-                ", nutrientContentStandardAmount='" + nutrientContentStandardAmount + '\'' +
-                ", energy=" + energy +
-                ", water=" + water +
-                ", protein=" + protein +
-                ", fat=" + fat +
-                ", ash=" + ash +
-                ", carbohydrate=" + carbohydrate +
-                ", sugar=" + sugar +
-                ", dietaryFiber=" + dietaryFiber +
-                ", calcium=" + calcium +
-                ", iron=" + iron +
-                ", phosphorus=" + phosphorus +
-                ", potassium=" + potassium +
-                ", sodium=" + sodium +
-                ", vitaminA=" + vitaminA +
-                ", retinol=" + retinol +
-                ", betaCarotene=" + betaCarotene +
-                ", thiamine=" + thiamine +
-                ", riboflavin=" + riboflavin +
-                ", niacin=" + niacin +
-                ", vitaminC=" + vitaminC +
-                ", vitaminD=" + vitaminD +
-                ", cholesterol=" + cholesterol +
-                ", saturatedFattyAcid=" + saturatedFattyAcid +
-                ", transFattyAcid=" + transFattyAcid +
-                ", sourceCode=" + sourceCode +
-                ", sourceName='" + sourceName + '\'' +
-                ", foodWeight='" + foodWeight + '\'' +
-                ", company='" + company + '\'' +
-                ", dataCreationMethodCode=" + dataCreationMethodCode +
-                ", dataCreationMethodName='" + dataCreationMethodName + '\'' +
-                ", dataCreationDate=" + dataCreationDate +
-                ", dataBaseDate=" + dataBaseDate +
-                ", providerCode=" + providerCode +
-                ", providerName='" + providerName + '\'' +
-                '}';
-    }
-
-
+    private String Kcal;
+    private String protein;
+    private String fat;
+    private String carbohydrate;
+    private String sugar;
+    private String sodium;
 
 }
-
