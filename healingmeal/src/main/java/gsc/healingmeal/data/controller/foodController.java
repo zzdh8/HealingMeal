853e244fc4322dbc164e99569dd98581c65c42e5
inhooo00 <chldlsgh0987@naver.com>
2024-01-06 01:39:20 +0900

    foodController 주석

--- healingmeal/src/main/java/gsc/healingmeal/data/controller/foodController.java
+++ healingmeal/src/main/java/gsc/healingmeal/data/controller/foodController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class foodController {
+public class foodController { //
 
     private final FoodService foodService;
 
